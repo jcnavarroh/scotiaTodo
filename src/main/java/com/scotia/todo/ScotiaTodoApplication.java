b3b3a57a33de384d97ee9dc6e4ba3215cd5e8218
Juan Camilo Navarro <ing.jcnavarroh@gmail.com>
2023-06-14 13:38:15 -0500

    Initial commit ScotiaTech Todo Project

---
+++ src/main/java/com/scotia/todo/ScotiaTodoApplication.java
@@ -0,0 +1,13 @@
+package com.scotia.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScotiaTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScotiaTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scotia/todo/ScotiaTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scotia.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScotiaTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
